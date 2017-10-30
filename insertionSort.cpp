1d942dde226b1ed9a0b534d519b914a75e39d851
Hetansh <hetansh4@gmail.com>
2017-10-30 17:22:48 -0400

    Insertion Sort Algorithm

---
+++ insertionSort.cpp
@@ -0,0 +1,46 @@
+/* CPE 593: HW 1a
+ * HETANSH MADHANI */
+
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+ void insertionSort(int x[], int n) {
+     int j;
+     int temp;
+    for (int i = 1; i < n; ++i) {
+        // insert x[i] into the list before it into the correct location
+                j = i;
+                while(j>0 && x[j]<x[j-1]){
+                    temp = x[j];
+                    x[j] = x[j-1];
+                    x[j-1] = temp;
+                    j--;
+                }
+    }
+ }
+ default_random_engine gen;
+
+ // fill an array of n elements with value from 0 to m
+ void randomArray(int x[], int n, int m) {
+    uniform_int_distribution<int> distribution(0, m);
+    for (int i = 0; i < n; ++i) {
+        x[i] = distribution(gen);
+    }
+ }
+
+ void print(int x[], int n) {
+    for (int i = 0; i < n; ++i)
+        cout << x[i] << ' ';
+    cout << '\n';
+ }
+
+ int main() {
+    const int n = 10;
+    int x[n];
+    randomArray(x, n, n);
+    print(x, n);
+    insertionSort(x, n);
+    print(x, n);
+ }
